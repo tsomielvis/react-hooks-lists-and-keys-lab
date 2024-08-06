353d775feec1f13772150c82694153cc9f86e298
tsomielvis <elvis.muthomi@studentmoringaschool.com>
2024-08-06 04:59:37 +0300

    completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,13 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+  <nav>
+  <a href="#home" key={links.home}>home</a>
+  <a href="#about" key={links.about}>about</a>
+  <a href="#projects" key={links.projects}>projects</a>
+  </nav>
+  )
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+
+  const oneTechnology = technologies.map ((namedTechnology) => {
+    return <span key = {namedTechnology}>{namedTechnology}</span>
+  })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
+      <span>{technologies}</span>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {oneTechnology}
       </div>
     </div>
   );
